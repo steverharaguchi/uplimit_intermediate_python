3fc8946dd39c75bae9db9fdcf4b7e51197e8b8fb
Wendy Ran Wei <wendyran@users.noreply.github.com>
2023-08-30 14:20:16 -0700

    Update data_processor.py

--- w1/data_processor.py
+++ w1/data_processor.py
@@ -79,5 +79,18 @@ class DataProcessor:
         aggregate should be 105.58
         """
         ######################################## YOUR CODE HERE ##################################################
+        # get generator from data_reader
+        data_reader_gen = #### [YOUR CODE HERE] ####
 
+        # skip first row as it is the column name
+        _ = next(data_reader_gen)
+
+        # initialize aggreate value
+        aggregate = #### [YOUR CODE HERE] ####
+
+        for row in tqdm(data_reader_gen):
+            if self.to_float(row[column_name]):
+                aggregate += #### [YOUR CODE HERE] ####
+
+        return aggregate
         ######################################## YOUR CODE HERE ##################################################
